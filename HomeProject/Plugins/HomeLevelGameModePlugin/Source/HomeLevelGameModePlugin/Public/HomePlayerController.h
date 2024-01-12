6f32c41c37c406b883ca5ab66933499430e93bda
SHsus1122 <116439064+SHsus1122@users.noreply.github.com>
2024-01-12 15:35:36 +0900

    한글 폰트 깨짐 해결(파일 형식 인코딩 65001 로 변경)

--- HomeProject/Plugins/HomeLevelGameModePlugin/Source/HomeLevelGameModePlugin/Public/HomePlayerController.h
+++ HomeProject/Plugins/HomeLevelGameModePlugin/Source/HomeLevelGameModePlugin/Public/HomePlayerController.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+О╩©// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -7,7 +7,7 @@
 #include "HomePlayerController.generated.h"
 
 /**
-* HomeLevel ©║╪╜ ╩Г©К╣г╢б PlayerController Class ют╢о╢ы.
+* HomeLevel Л≈░Л└° Л┌╛Л ╘К░≤К┼■ PlayerController Class Л·┘К▀┬К▀╓.
 */
 UCLASS()
 class HOMELEVELGAMEMODEPLUGIN_API AHomePlayerController : public APlayerController
