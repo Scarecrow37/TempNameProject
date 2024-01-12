ab010f7e9d91b35a81ec682890bdae56a64dc2f8
SHsus1122 <tjgurtn1122@naver.com>
2024-01-12 15:22:47 +0900

    PlayerController 기본 뼈대 작성

---
+++ HomeProject/Plugins/HomeLevelGameModePlugin/Source/HomeLevelGameModePlugin/Public/HomePlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HomePlayerController.generated.h"
+
+/**
+* HomeLevel 에서 사용되는 PlayerController Class 입니다.
+*/
+UCLASS()
+class HOMELEVELGAMEMODEPLUGIN_API AHomePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
