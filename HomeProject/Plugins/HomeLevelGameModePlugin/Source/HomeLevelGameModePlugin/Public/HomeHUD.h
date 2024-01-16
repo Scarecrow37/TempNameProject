d61f0fe29dd69b7778510e5462a902ccfad7d481
polli789 <polli789@naver.com>
2024-01-16 18:39:18 +0900

    [FEAT] HomeHUD 추가

---
+++ HomeProject/Plugins/HomeLevelGameModePlugin/Source/HomeLevelGameModePlugin/Public/HomeHUD.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HomeHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOMELEVELGAMEMODEPLUGIN_API AHomeHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+
+protected:
+
+private:
+
+};
