1d772200fbc769ede7e4debe6e79729bfc2a334f
Jun Ahn <brigdesatis@gmail.com>
2024-01-26 10:29:14 +0900

    240126 // 게임 인트로 플레이어 컨트롤러 및 위젯 제작 (PM 요청으로 플러그인 X)

---
+++ WWH_IntroScreen/Source/WWH_IntroScreen/Private/IntroScreenWidget.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IntroScreenWidget.h"
+#include "Blueprint/UserWidget.h"
+
+
+void UIntroScreenWidget::OnVideoEnd_Implementation()
+{
+}
+
