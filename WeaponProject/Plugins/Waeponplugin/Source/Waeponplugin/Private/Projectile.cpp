5f3e03c0a6b9b3d644ddd3f566d0b0585ba8a7c5
polli789 <polli789@naver.com>
2024-01-31 16:59:03 +0900

    [FEAT] 총,총알 (생성,붙이기,발사,리플리케이트 등 )

---
+++ WeaponProject/Plugins/Waeponplugin/Source/Waeponplugin/Private/Projectile.cpp
@@ -0,0 +1,39 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Projectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/SphereComponent.h"
+
+Projectile::Projectile()
+{
+	this->SetReplicates(true);
+	// Use a sphere as a simple collision representation
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	CollisionComp->InitSphereRadius(5.0f);
+	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
+	CollisionComp->OnComponentHit.AddDynamic(this, &Projectile::OnHit);		// set up a notification for when this component hits something blocking
+
+	RootComponent = CollisionComp;
+
+	// 무브먼트 세팅
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
+	ProjectileMovement->UpdatedComponent = CollisionComp;
+	ProjectileMovement->InitialSpeed = 3000.f;
+	ProjectileMovement->MaxSpeed = 3000.f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = true;
+
+	// 3초후 자동 파괴
+	InitialLifeSpan = 3.0f;
+}
+
+void Projectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	// hit 시 파괴
+	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
+	{
+		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
+
+		Destroy();
+	}
+}
