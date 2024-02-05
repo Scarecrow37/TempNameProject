482e11aac69764601f51c2ebbea7db0b7bf512f5
Suha Do <weqa123@naver.com>
2024-02-05 12:46:29 +0900

    Create Widgets.

---
+++ MainProject/Source/TempNameProject/Public/UMG/LabeledInput.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LabeledInput.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEMPNAMEPROJECT_API ULabeledInput : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
